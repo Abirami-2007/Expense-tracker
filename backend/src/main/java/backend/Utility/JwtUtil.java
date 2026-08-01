2c6596f1395097294d91d46e52271bec114dfef4
abirami-2007 <abiramiabirami9626@gmail.com>
2026-08-01 15:19:36 +0530

    update utility

--- backend/src/main/java/backend/Utility/JwtUtil.java
+++ backend/src/main/java/backend/Utility/JwtUtil.java
@@ -1,6 +1,6 @@
-
 package backend.Utility;
 
+import org.springframework.beans.factory.annotation.Value;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
